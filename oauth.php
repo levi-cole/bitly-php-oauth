147edf798473fd5364d5d2043dafc3641a25c7e6
levi-cole <levi.cole@silver-worldwide.com>
2016-02-11 14:35:51 +0000

    Update oauth.php
    Code reduction, taking advantage of http_build_query() function to replace foreach ($params as $key => $value)

--- oauth.php
+++ oauth.php
@@ -1,34 +1,22 @@
 <?php
+	$params = [
+		'client_id' => '', // Enter your Client ID
+		'client_secret' => '', // Enter your Client Secret
+		'code' => $_GET['code'],
+		'redirect_uri' => 'http://.../oauth.php' // The redirect_uri is just pointing back to the location of this file
+	];
 
-	$code = $_REQUEST['code'];
-
-	// Enter your Client ID, Client Secret and Redirect URI
-	$client_id ="";
-	$client_secret = "";
-	$redirect_uri = "http://.../oauth.php"; //The redirect_uri is just pointing back to the location of this file
-
-	$uri = "https://api-ssl.bitly.com/oauth/access_token"; 
-	
-	//POST to the bitly authentication endpoint
-	$params = array();
-    	$params['client_id'] = $client_id;
-    	$params['client_secret'] = $client_secret;
-    	$params['code'] = $code;
-    	$params['redirect_uri'] = $redirect_uri;
-	
-	$output = "";
-  	$params_string = "";
-  	foreach ($params as $key=>$value) { $params_string .= $key.'='.$value.'&'; }
-  		rtrim($params_string,'&');
-  	try {
+	$output = '';
+	try {
 		$ch = curl_init();
-		curl_setopt($ch,CURLOPT_URL, $uri);
-		curl_setopt($ch,CURLOPT_POST, count($params));
-		curl_setopt($ch,CURLOPT_POSTFIELDS, $params_string);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	    $output = curl_exec($ch);
-  	} catch (Exception $e) {
-  	}
+			curl_setopt($ch, CURLOPT_URL, 'https://api-ssl.bitly.com/oauth/access_token');
+			curl_setopt($ch, CURLOPT_POST, count($params));
+			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$output = curl_exec($ch);
+	} catch (Exception $e) {
+		echo $e->getMessage();
+	}
 	// Returns a URL encoded string in the format of "access_token=%s&login=%s&apiKey=%s"
 	
 	// parse_str() creates variables $access_token, $login, $apiKey (http://php.net/manual/en/function.parse-str.php)
@@ -42,8 +30,3 @@
 
 	//echo $apiKey;  
 	//API keys are deprecated. This value will be removed in the future.
- 
-?>
-
-
-
